2624acdde76aa4a3121a58cb08e3a9b8dd73dc26
flexzero <flexzero2016@gmail.com>
2019-12-01 13:54:16 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// window.onscroll = () => {
+//     const header = document.querySelector('#header');
+//     if(this.scrollY <= 30) header.className = ''; else header.className = 'scroll-header';
+//   };
